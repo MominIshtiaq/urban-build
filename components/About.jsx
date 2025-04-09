9eadb7001daa7f1c263b230aee70be8ecc7b3f22
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-04-09 16:18:56 +0500

    Stats and Service layout complete

--- components/About.jsx
+++ components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <section className="pt-16 xl:pt-32" id="about">
       <div className="container mx-auto">
         <div className="flex flex-col gap-12 xl-gap:0 xl:flex-row xl:items-center">
-          <div className="flex-1 flex justify-center">
+          <div className="flex-1 xl:flex xl:justify-center">
             <div className="max-w-[540px]">
               <Pretitle text="About us" />
               <h2 className="h2 mb-6">
@@ -32,13 +32,14 @@ const About = () => {
             </div>
           </div>
           <div className="flex-1 xl:flex xl:justify-center">
-            <div className="xl:w-[444px] xl:h-[493px] relative">
+            <div className="w-full xl:w-[444px] xl:h-[493px] relative">
               <div className="hidden xl:flex w-[444px] h-[493px] bg-[#ffca3b] absolute -top-4 -left-4 -z-10"></div>
               <Image
                 src="/assets/img/about/img.jpg"
+                alt="Img"
                 width={444}
                 height={492}
-                alt="Img"
+                style={{ width: "100%", height: "492px", objectFit: "cover" }}
               />
             </div>
           </div>
--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
+import { useInView } from "motion/react";
+import CountUp from "react-countup";
+
+const statsData = [
+  {
+    endCountNum: 99,
+    endCountText: "%",
+    text: "Client Statisfaction",
+  },
+  {
+    endCountNum: 800,
+    endCountText: "+",
+    text: "Successfull Projects",
+  },
+  {
+    endCountNum: 32,
+    endCountText: "k",
+    text: "Happy Clients",
+  },
+  {
+    endCountNum: 26,
+    endCountText: "+",
+    text: "Years of Experience",
+  },
+];
 
 const Stats = () => {
-  return <div>Stats</div>;
+  const ref = useRef(null);
+  const inView = useInView(ref, { treshold: 0.2 });
+  return (
+    <div ref={ref} className="mt-15 xl:mt-32 bg-[#121315] py-10 w-full">
+      <div className="container mx-auto h-full">
+        <div className="text-white flex flex-col items-center justify-around xl:flex-row h-full gap-12 text-center xl:text-left">
+          {statsData.map((item, index) => {
+            return (
+              <div key={index}>
+                <div className="text-5xl font-semibold">
+                  {inView && (
+                    <CountUp
+                      start={0}
+                      end={item.endCountNum}
+                      delay={0.5}
+                      duration={3}
+                    />
+                  )}
+                  <span>{item.endCountText}</span>
+                </div>
+                <p>{item.text}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
